bc7f9dbc53ff1563efa695d8cd9500c2fc23583d
Faeest <faeest.115@gmail.com>
2023-12-07 20:09:49 +0700

    maze generator, bugged alot :(

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let game;
+let currentFPS = 0;
 function preload() {}
 
 function setup() {
@@ -9,7 +10,16 @@ function setup() {
 
 function draw() {
 	background(0);
+	renderFPS();
 	game.render();
+}
+function renderFPS() {
+	if (frameCount % 10 == 0) {
+		currentFPS = round(frameRate());
+	}
+	push();
+	fill(255).noStroke().strokeWeight(0).text(currentFPS, 10, 20);
+	pop();
 }
 function keyReleased() {
 	// WASD 87 65 83 68
@@ -35,9 +45,9 @@ function keyReleased() {
 
 class Game {
 	constructor() {
-		this.res = 64;
+		this.res = 48;
+		this.radius = (this.res > 64 ? 4 : this.res >= 48 ? 8 : this.res >= 32 ? 12 : this.res >= 16 ? 16 : this.res >= 8 ? 32 : 64) + 1;
 		this.map = new Map(this.res);
-		this.map.generate();
 		this.player = new Player({ res: this.res, tile: _.filter(_.flatten(this.map.tiles), { center: true }) });
 		this.cam = new Camera();
 		this.state = "preparation"; // "preparation" "started" "over"
@@ -54,6 +64,9 @@ class Camera {
 		this.tilt = createVector(0, 0);
 	}
 	render(playerInstance, mapInstance) {
+		if (game.state == "preparation" && frameCount % 5 == 0) {
+			// mapInstance.generate();
+		}
 		if (
 			frameCount % 5 == 0 &&
 			(!playerInstance.moving || abs(playerInstance.pos.x + this.tilt.x - this.anchor.x) > width * 0.4 || abs(playerInstance.pos.y + this.tilt.y - this.anchor.y) > height * 0.4) &&
@@ -66,38 +79,49 @@ class Camera {
 		rectMode(CENTER);
 		mapInstance.render(this.tilt);
 		playerInstance.render(this.tilt);
+		mapInstance.renderCollision(this.tilt);
 		pop();
 	}
 }
 class Player {
 	constructor({ res = 64, tile }) {
 		this.ontile = (tile.length && tile.length > 1) || tile ? tile[0] : undefined;
-		console.log(this.ontile);
 		this.res = res;
+		this.radius = (this.res > 64 ? 4 : this.res >= 48 ? 8 : this.res >= 32 ? 12 : this.res >= 16 ? 16 : this.res >= 8 ? 32 : 64) + 1;
 		this.pos = createVector(this.ontile.pos.x, this.ontile.pos.y);
 		this.queue = [];
 		this.moving = false;
 	}
 	render(vector) {
 		push();
-		fill("red").rect((vector.x ?? 0) + this.pos.x, (vector.y ?? 0) + this.pos.y, this.res, this.res);
+		fill("red")
+			.noStroke()
+			.rect((vector.x ?? 0) + this.pos.x, (vector.y ?? 0) + this.pos.y, this.res, this.res);
 		pop();
 		if (!this.moving && this.queue.length) {
 			this.moving = true;
 			let dir = this.queue.shift();
-			let newIndex = this.ontile.index + game.map.radius * dir[0] + dir[1];
+			let newIndex = this.ontile.index + this.radius * dir[1] + dir[0];
+			let destination = _.flatten(game.map.tiles)[newIndex];
 			if (
 				newIndex < 0 ||
 				newIndex > _.flatten(game.map.tiles).length - 1 ||
-				(floor(this.ontile.index / game.map.radius) == 0 && dir[0] < 0) ||
-				(ceil(this.ontile.index / game.map.radius) == game.map.radius && dir[0] > 0)
+				(floor(this.ontile.index / this.radius) == 0 && dir[1] < 0) ||
+				(ceil(this.ontile.index / this.radius) == this.radius && dir[1] > 0) ||
+				(this.ontile.index % this.radius == 0 && dir[0] < 0) ||
+				(this.ontile.index % this.radius == 8 && dir[0] > 0) ||
+				game.state == "started" ||
+				(dir[0] > 0 && (destination.collide.left || this.ontile.collide.right)) || //* right
+				(dir[0] < 0 && (destination.collide.right || this.ontile.collide.left)) || //* left
+				(dir[1] > 0 && (destination.collide.top || this.ontile.collide.bottom)) || //* bottom
+				(dir[1] < 0 && (destination.collide.bottom || this.ontile.collide.top)) //* top
 			) {
 				newIndex = this.ontile.index;
 			}
 			if (newIndex == this.ontile.index) {
 				this.moving = false;
 			} else {
-				this.ontile = _.flatten(game.map.tiles)[newIndex];
+				this.ontile = destination;
 				gsap.to(this.pos, {
 					x: this.ontile.pos.x,
 					y: this.ontile.pos.y,
@@ -117,25 +141,74 @@ class Map {
 	constructor(res = 64) {
 		this.res = res;
 		this.tiles = [];
-		this.radius = (res >= 64 ? 8 : res < 64 ? 12 : res < 32 ? 18 : 24) + 1;
-	}
-	generate(difficulty = 1) {
+		this.radius = (res > 64 ? 4 : res >= 48 ? 8 : res >= 32 ? 12 : res >= 16 ? 16 : res >= 8 ? 32 : 64) + 1;
 		for (let i = 0; i < this.radius; i++) {
 			let row = [];
 			for (let u = 0; u < this.radius; u++) {
-				row.push(new Tiles({ index: i * this.radius + u, res: this.res, x: i * this.res, y: u * this.res, center: this.radius - (i - 1) == i && this.radius - (u - 1) == u }));
+				row.push(new Tiles({ index: i * this.radius + u, res: this.res, x: u * this.res, y: i * this.res, center: (pow(this.radius, 2) - 1) * 0.5 == i * this.radius + u }));
 			}
 			this.tiles.push(row);
 		}
+		this.flattenTiles = _.flatten(this.tiles);
+	}
+	async generate() {
+		if (this.flattenTiles.length == 0 || game.state != "preparation") return;
+		let getDirection = ({ r = false, l = false, t = false, b = false }) => {
+			let availableIndex = [];
+			if (!r) availableIndex.push("right");
+			if (!l) availableIndex.push("left");
+			if (!t) availableIndex.push("top");
+			if (!b) availableIndex.push("bottom");
+			return availableIndex[_.floor(_.random(0, availableIndex.length - 1))];
+		};
+		let checkPath = (tile) => {
+			return {
+				r: _.includes(_.concat(passed, generated), tile.index + 1) || current.index % this.radius == 8,
+				l: _.includes(_.concat(passed, generated), tile.index - 1) || current.index % this.radius == 0,
+				b: _.includes(_.concat(passed, generated), tile.index + this.radius) || ceil(current.index / this.radius) == this.radius,
+				t: _.includes(_.concat(passed, generated), tile.index - this.radius) || floor(current.index / this.radius) == 0,
+			};
+		};
+		let getTile = (index) => this.tiles[floor(index / this.radius)][index % this.radius];
+		let passed = [];
+		let generated = [];
+		let generating = true;
+		let current = game.player.ontile;
+		while (generating) {
+			await new Promise((r) => setTimeout(r, 100));
+			generating = generated.length != this.flattenTiles.length;
+			let path = checkPath(current);
+			let dir = getDirection(path);
+			let destinationTile = this.flattenTiles[current.index + (dir == "right" ? 1 : dir == "left" ? -1 : dir == "bottom" ? this.radius : dir == "top" ? -this.radius : 0)];
+			if (!_.includes(path, false)) {
+				let lastTile = passed.pop();
+				current = getTile(lastTile);
+				generated.push(lastTile);
+				console.log("locked up", current);
+			} else {
+				passed.push(current.index);
+				getTile(current.index).collide[dir] = false;
+				getTile(destinationTile.index).collide[dir == "right" ? "left" : dir == "left" ? "right" : dir == "bottom" ? "top" : "bottom"] = false;
+				console.log(generated, passed, dir, path, destinationTile, current);
+				current = destinationTile;
+			}
+			this.flattenTiles = _.flatten(this.tiles);
+		}
 	}
 	render(vector) {
-		_.flatten(game.map.tiles).forEach((e) => {
+		this.flattenTiles.forEach((e) => {
 			e.render(vector);
+		});
+	}
+	renderCollision(vector) {
+		if (this.flattenTiles.filter((e) => _.includes(e.collide, true)).length == 0) return;
+		this.flattenTiles.forEach((e) => {
+			e.renderCollision(vector);
 		});
 	}
 }
 class Tiles {
-	constructor({ r = false, l = false, t = false, b = false, center = false, x = 0, y = 0, res = 64, index = false }) {
+	constructor({ r = true, l = true, t = true, b = true, center = false, x = 0, y = 0, res = 64, index = false }) {
 		if (index != undefined) {
 			this.index = index;
 		}
@@ -150,11 +223,31 @@ class Tiles {
 		this.center = center;
 	}
 	render(vector) {
+		let final = [(vector.x ?? 0) + this.pos.x, (vector.y ?? 0) + this.pos.y];
 		push();
-		stroke(50)
-			.noFill()
-			.strokeWeight(1)
-			.rect((vector.x ?? 0) + this.pos.x, (vector.y ?? 0) + this.pos.y, this.res, this.res);
+		stroke(50).noFill().strokeWeight(1).rect(final[0], final[1], this.res, this.res);
+		text(this.index, final[0] - textWidth(this.index) * 0.5, final[1] + 5);
+		pop();
+	}
+	renderCollision(vector) {
+		let final = [(vector.x ?? 0) + this.pos.x, (vector.y ?? 0) + this.pos.y];
+		push();
+		final[0] -= this.res * 0.5;
+		final[1] -= this.res * 0.5;
+		stroke(255);
+		strokeWeight(2);
+		if (this.collide.left) {
+			line(final[0], final[1], final[0], final[1] + this.res);
+		}
+		if (this.collide.right) {
+			line(final[0] + this.res, final[1], final[0] + this.res, final[1] + this.res);
+		}
+		if (this.collide.top) {
+			line(final[0], final[1], final[0] + this.res, final[1]);
+		}
+		if (this.collide.bottom) {
+			line(final[0], final[1] + this.res, final[0] + this.res, final[1] + this.res);
+		}
 		pop();
 	}
 }
